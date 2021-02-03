fafff8e7849828536846de50ef2147efed4c3359
marintelli <lyh.enoch@gmail.com>
2021-02-03 15:56:18 +0900

    unit 13 증감연산자.

---
+++ unit13.4_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 2;
+    int num2 = 2;
+    int num3;
+    int num4;
+
+    num3 = num1++;
+    num4 = num2--;
+
+
+    printf("%d %d\n", num3, num4);
+    printf("%d %d\n", num1, num2);    
+    return 0 ; 
+}
+
+/*
+
+*/
