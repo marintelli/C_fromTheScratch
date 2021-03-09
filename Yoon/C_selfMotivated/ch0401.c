68e8c914466007db39f5edda372fb01c722e6497
marintelli <lyh.enoch@gmail.com>
2021-03-09 18:18:29 +0900

    chapter 4

---
+++ Yoon/C_selfMotivated/ch0401.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    double a,b;
+    int c, d;
+
+    scanf("%lf %lf", &a, &b);
+   
+   
+    c = a + b ;
+    // 실수 두개를 더하여 정수 변수에 대입
+
+    d = (int)a + (int)b ;
+     // 두개의 실수를 각각 정수로 변환하여 정수 변수에 대입
+
+    printf(" %d %d \n", a, b);
+    // 결과값을 출력하고 비교해본다. 
+
+    return 0;
+}
