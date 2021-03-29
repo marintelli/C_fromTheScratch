42359e5878de261507bce9addd9a339d3b165d65
marintelli <58841571+marintelli@users.noreply.github.com>
2021-03-29 19:41:46 +0900

    Update ch1001.c

--- C_selfMotivated/ch1001.c
+++ C_selfMotivated/ch1001.c
@@ -3,17 +3,18 @@
 int main()
 {
 	char x;
-	int characterArr[255] = { 0, };
+	int characterArr[255] = { 0, }; //문자의 주소지는 보통 총 255개이므로, 배열에 255칸
 
 	while (1) {
-		scanf(" %c", &x);
-		if (x < 'A' || x > 'Z') break;
-		characterArr[x]++;
+		scanf(" %c", &x); // 
+		if (x < 'A' || x > 'Z') break; // A부터 Z를 넘어가면 실행 종료
+		characterArr[x]++; // 
 	}
 
 	for (int i = 'A'; i <= 'Z'; i++) {
 
-		if(characterArr[i] != 0) printf("%c : %d \n", i, characterArr[i]);
+		if(characterArr[i] != 0) printf("%c : %d \n", i, characterArr[i]); 
+		// 배열의 숫자가 0이 아닌 경우의 배열을 불러서 프린트해준다.
 	}
 
 	return 0; 
