881d04512e6b8d6e08fa4e89c6befde013240558
marintelli <58841571+marintelli@users.noreply.github.com>
2021-04-27 14:17:11 +0900

    형성평가1204

---
+++ C_selfMotivated/형성평가(복습)/test1204.c
@@ -0,0 +1,31 @@
+// 4번 
+
+#include <stdio.h> 
+
+#include <math.h> 
+
+  
+
+int main() 
+
+{ 
+
+         
+
+        double n; 
+
+         
+
+        scanf("%lf", &n); 
+
+  
+
+        printf("%.0lf", pow(2,n)); 
+
+  
+
+  
+
+  
+
+} 
