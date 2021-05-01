6fdc146a885d88f951fc6c2296ffbfe274500f58
marintelli <58841571+marintelli@users.noreply.github.com>
2021-05-02 08:06:05 +0900

    자가진단 1403

---
+++ C_selfMotivated/자가진단(예습)/ch1403.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main()
+{
+    
+    char str[50] = "Hong Gil Dong";
+        
+    for (int i = 3 ; i < 7; i++) {
+        printf("%c", str[i]);
+    }
+    
+    
+    printf("\n");
+    
+    return 0;
+
+}
