0fa5b31643e871b04acceace6a28350affa6339a
marintelli <58841571+marintelli@users.noreply.github.com>
2021-05-18 13:36:59 +0900

    Create ch1505.c

---
+++ C_selfMotivated/자가진단(예습)/ch1505.c
@@ -0,0 +1,21 @@
+// 1505
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char st1[50];
+    char st2[50] = "fighting";
+    
+    
+    scanf("%s", st1);
+    
+    
+    strcat(st1, st2);
+    
+    puts(st1);
+
+    
+    return 0;
+    
+}
