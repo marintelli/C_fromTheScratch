200a7f1ed76052530ed0406664502dc52c0a2a90
marintelli <58841571+marintelli@users.noreply.github.com>
2021-05-18 13:20:02 +0900

    Update test1502.c

--- C_selfMotivated/형성평가(복습)/test1502.c
+++ C_selfMotivated/형성평가(복습)/test1502.c
@@ -1,19 +1,2 @@
 //1502
 
-#include <stdio.h>
-#include <string.h>
-
-
-int main()
-{
-    char st1[50];
-    char st2[50];
-    
-    
-    fgets(st1,50,stdin);
-    
-    strcpy(st2, st1);
-    
-    printf("%s \n", st2);
-    
-}
