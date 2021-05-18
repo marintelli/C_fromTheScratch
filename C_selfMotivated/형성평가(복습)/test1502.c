86f91f1b56bf919a64f9b44cc402d0d23c5177c5
marintelli <58841571+marintelli@users.noreply.github.com>
2021-05-18 14:43:26 +0900

    형성평가 1502

--- C_selfMotivated/형성평가(복습)/test1502.c
+++ C_selfMotivated/형성평가(복습)/test1502.c
@@ -1,2 +1,24 @@
+
 //1502
 
+#include <string.h>
+
+int main()
+{
+    char ch;
+    char str[5][50]= {"flower", "rose", "lily","daffodil","azalea" };
+    int cnt = 0;
+    scanf(" %c", &ch);
+    
+    for (int i= 0; i<5; i++) {
+        if( str[i][1] == ch || str[i][2] == ch)
+        {
+            puts(str[i]);
+            cnt++;
+        }
+    }
+    
+    printf("%d\n", cnt);
+    
+    return 0;
+}
