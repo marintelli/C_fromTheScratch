404b57f2ecd64fca8269ba16139bd8a31b50824e
marintelli <58841571+marintelli@users.noreply.github.com>
2021-05-18 16:51:41 +0900

    자가진단 1511

---
+++ C_selfMotivated/자가진단(예습)/ch1511.c
@@ -0,0 +1,20 @@
+//자가진단 1511
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+    int a, b, c, d, e;
+    char str[200];
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    sprintf(str, "%d%d%d%d%d", a, b, c,d,e);
+    
+    int i, len = strlen(str);
+    
+    for (i = 0; i < len; i++) {
+        
+        if ((i%3) == 0) printf("\n");
+        printf("%c", str[i]);
+        
+    }
+    return 0; }
