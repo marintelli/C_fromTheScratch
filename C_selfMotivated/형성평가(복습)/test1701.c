c2adf6e038da70d44d6d4f879b29d31310072163
marintelli <58841571+marintelli@users.noreply.github.com>
2021-05-31 17:27:17 +0900

    Create test1701.c

---
+++ C_selfMotivated/형성평가(복습)/test1701.c
@@ -0,0 +1,22 @@
+//1701
+
+#include <stdio.h>
+
+int main()
+{
+
+	int i;
+	double f;
+	
+
+	int* pi = &i;
+	double* pf = &f;
+
+	printf("%#p \n", pi);
+	printf("%#p \n", pf);
+
+
+
+
+	return 0;
+}
